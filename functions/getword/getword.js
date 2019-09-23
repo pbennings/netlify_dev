6ac9cd4cbb09321e5a758f0a520bb1410715c9ed
Paul Bennings <>
2019-09-23 16:28:08 -0400

    testing sample

---
+++ functions/getword/getword.js
@@ -0,0 +1,16 @@
+const randomWords = require('random-words')
+
+async function getWord() {
+  return Promise.resolve(randomWords().toUpperCase());
+}
+
+exports.handler = async function (event, context) {
+  try {
+      const body = await getWord();
+      return {
+      statusCode: 200,
+      body };
+   } catch (err) {
+      return { statusCode: 500, body: err.toString() };
+  }
+};
